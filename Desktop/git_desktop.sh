eda639a935428feb4d2bdeb1c53549db47046c35
yann hiroshi <holdless2@gmail.com>
2016-09-24 12:19:53 +0800

    Desktop/

--- Desktop/git_desktop.sh
+++ Desktop/git_desktop.sh
@@ -6,6 +6,11 @@ git add /home/pi/Desktop/sys backup/*
 git add /home/pi/Desktop/tango/dt/*
 git add /home/pi/Desktop/tango/inte/*
 
+git add /Desktop/_api/*
+git add /Desktop/_gcc/*
+git add /Desktop/arena/*
+git add /Desktop/_cmd/*
+
 
 git commit -m "Desktop/"
 git push -u origin master
