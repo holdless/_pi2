311479727a7d0577381b67cd30f7463438a64583
yann hiroshi <holdless2@gmail.com>
2016-09-27 10:34:05 +0800

    Desktop/

--- Desktop/git_desktop.sh
+++ Desktop/git_desktop.sh
@@ -1,5 +1,6 @@
 cd
 
+# add directories or files you'd like to add to commit to github
 git add /home/pi/Desktop/*.*
 git add /home/pi/Desktop/sys backup/*
 
@@ -11,6 +12,6 @@ git add /home/pi/Desktop/_gcc/*
 git add /home/pi/Desktop/arena/*
 git add /home/pi/Desktop/_cmd/*
 
-
+# commit and push to github
 git commit -m "Desktop/"
 git push -u origin master
