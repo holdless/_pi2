708ae7ea7075e2fd42c4f8900dc424b297f01953
yann hiroshi <holdless2@gmail.com>
2017-01-17 19:14:06 +0800

    Desktop/

--- Desktop/git_desktop.sh
+++ Desktop/git_desktop.sh
@@ -3,6 +3,12 @@ cd
 # add directories or files you'd like to add to commit to github
 git add /home/pi/Desktop/*.*
 git add /home/pi/Desktop/sys_backup/*
+git add /home/pi/Desktop/config/*
+git add /home/pi/Desktop/_info/*
+git add /home/pi/Desktop/_api/*
+git add /home/pi/Desktop/_gcc/*
+git add /home/pi/Desktop/arena/*
+git add /home/pi/Desktop/_cmd/*
 
 # tango project
 git add /home/pi/Desktop/tango/dt/*
@@ -10,11 +16,6 @@ git add /home/pi/Desktop/tango/inte/*
 git add /home/pi/Desktop/tango/remote/*.*
 git add /home/pi/Desktop/tango/remote/icons/*
 git add /home/pi/Desktop/tango/remote/images/*
-
-git add /home/pi/Desktop/_api/*
-git add /home/pi/Desktop/_gcc/*
-git add /home/pi/Desktop/arena/*
-git add /home/pi/Desktop/_cmd/*
 
 # commit and push to github
 git commit -m "Desktop/"
